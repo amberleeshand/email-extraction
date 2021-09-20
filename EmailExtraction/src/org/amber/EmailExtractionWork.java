f74b06de4f395c27c57f2090e7a4373b1a2d2952
Amber Shand <amber.shand@aexp.com>
2021-09-20 10:33:32 +0100

    attempted to follow daves solution

--- EmailExtraction/src/org/amber/EmailExtractionWork.java
+++ EmailExtraction/src/org/amber/EmailExtractionWork.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,13 +28,21 @@ public class EmailExtractionWork {
         int counter = 0;
         String contents = Files.readString(filePath);
         //regex to find all of the Softwire email addresses
-        String regex = "[a-zA-Z0-9_\\-\\.]+[@]+softwire\\.com\\s";
+//        String regex = "[a-zA-Z0-9_\\-\\.]+[@]+softwire\\.com\\s";
+        String regex = "(?<name>.[\\w_]+)@(?<domain>(?:[\\w\\-.]+)\\.)+([\\w\\-.]+)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(contents);
 
+        Set<String> domainSet = new HashSet<String>();
         while (matcher.find()) {
-            System.out.println(matcher.group(0));
-            counter++;
+            String domain = matcher.group("domain");
+            domainSet.add(domain);
+//            System.out.println(matcher.group("domain"));
+//            counter++;
+        }
+
+        for (String oneDomain: domainSet) {
+//            System.out.println(oneDomain);
         }
 
 //        if (matcher.find()) {
