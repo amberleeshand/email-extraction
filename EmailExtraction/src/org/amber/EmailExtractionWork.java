402a21b844db82d8f615ec707eb759acf9dc23b2
Amber Shand <amber.shand@aexp.com>
2021-09-20 11:59:45 +0100

    Sorted the email extraction to find the order of email occurrences from highest to lowest

--- EmailExtraction/src/org/amber/EmailExtractionWork.java
+++ EmailExtraction/src/org/amber/EmailExtractionWork.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -13,7 +14,7 @@ public class EmailExtractionWork {
 
     public static void main(String[] args) {
         String fileName = "/Users/acshand/IdeaProjects/email-extraction/EmailExtraction/sample.txt";
-        if (args.length > 1){
+        if (args.length > 1) {
             fileName = args[1];
         }
         try {
@@ -26,24 +27,40 @@ public class EmailExtractionWork {
     private static void searchFile(String fileName) throws IOException {
         Path filePath = Paths.get(fileName);
 
-        int counter = 0;
+//        int counter = 0;
         String contents = Files.readString(filePath);
-        //regex to find all of the Softwire email addresses
-        String regex = "[a-zA-Z0-9_\\-\\.]+[@]+softwire\\.com\\s";
+        String regex = "[a-zA-Z0-9_\\-.]+@(?<domain>[a-z]+(\\.[a-z]+)+)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(contents);
 
+        Map<String, Integer> domains = new HashMap<>();
+
         while (matcher.find()) {
-            System.out.println(matcher.group(0));
-            counter++;
+//            counter++;
+
+            String key = matcher.group("domain");
+            domains.put(key, domains.getOrDefault(key, 0) + 1);
+
+        }
+        // Amount of email occurrences per email
+        domains.forEach(
+                (email, emailOccurrence) -> System.out.printf("%s: %d%n", email, emailOccurrence));
+
+        System.out.println(domains);
+        System.out.println("-----------");
+        // Sorted to find the order from highest to lowest
+
+        domains.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(e -> System.out.println(e));
+//        System.out.println(sorted);
+//        System.out.printf("Found %s %d times", regex, counter);
+        System.out.println(domains);
         }
 
-//        if (matcher.find()) {
-//            counter++;
-//            System.out.println("Found here: "+ matcher.group());
-//        }
-        System.out.printf("Found %s %d times", regex, counter);
-    }
+//    Switch it to a Linked Hashmap (reg ones don't get sorted) - use streams and lambdas
+
 
 
 }
